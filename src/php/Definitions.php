2fb3915bf30885caa403120fdd7a2d436c193dca
Ole Trenner <ole@jayotee.de>
2021-06-16 09:30:40 +0200

    adjusted format definitions

--- src/php/Definitions.php
+++ src/php/Definitions.php
@@ -18,19 +18,19 @@ class Definitions {
                 'title' => 'Avery Zweckform 18x10mm (transparent foil)',
                 'layout' => 'columns',
 
-                'label_height' => 9.7,
+                'label_height' => 10,
                 'label_width' => 17.45,
-                'label_padding' => 0.5,
+                'label_padding' => 1,
                 'labels_per_row' => 10,
                 'labels_per_page' => 270,
                 'gap_left' => 2.9,
-                'gap_top' => 0.3,
+                'gap_top' => 0,
                 'fontsize' => 6,
 
                 'margins' => array(
-                    'top' => 13,
-                    'left' => 4.5,
-                    'right' => 4.5,    //obsolete
+                    'top' => 12.7,
+                    'left' => 4.9,
+                    'right' => 4.9,    //obsolete
                     'bottom' => 13,  //obsolete
                 ),
             ),
@@ -43,12 +43,12 @@ class Definitions {
                 'label_padding' => 1,
                 'labels_per_row' => 8,
                 'labels_per_page' => 96,
-                'gap_left' => 4,
-                'gap_top' => 4,
+                'gap_left' => 4.1,
+                'gap_top' => 4.1,
                 'fontsize' => 7,
 
                 'margins' => array(
-                    'top' => 6,
+                    'top' => 6.5,
                     'left' => 12,
                     'right' => 12,    //obsolete
                     'bottom' => 6,  //obsolete
